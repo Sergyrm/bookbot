55f00ea00c33ae466da824ded2e993c4e81be91c
Sergyrm <sergy.r.m@gmail.com>
2025-01-28 16:43:25 +0100

    added word and char count in main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 def main():
     with open("books/frankenstein.txt") as file_to_read:
         content = file_to_read.read()
-    print(content)
+    
+    print(char_count(content))
+    
+def word_count(book: str):
+    return len(book.split())
+
+def char_count(book: str):
+    char_dict = {}
+
+    for ch in book.lower():
+        if ch not in char_dict:
+            char_dict[ch] = book.lower().count(ch)
+
+    return char_dict
 
 main()
